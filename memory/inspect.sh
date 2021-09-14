01528d510933a27460a5c844c4cc3b77e8d7739b
superxcgm <superxcgm@gmail.com>
2021-09-14 21:40:56 +0800

    add mem-layout

---
+++ memory/inspect.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+set -e
+
+gcc mem-layout.c -o mem-layout
+size mem-layout
---
+++ memory/mem-layout.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int global1_bss;
+int global2_bss;
+
+int global_data = 5;
+
+int main(void) {
+    return 0;
+}
